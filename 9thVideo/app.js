16e8cec2f74659d98fa798d06cc2b583efd38dbe
Ankit <ankitmaurya9560@gmail.com>
2024-07-09 09:15:21 +0530

    Auth and jwt

--- 9thVideo/app.js
+++ 9thVideo/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.use(cookieParser());
+
+
+
+app.get('/', function (req, res) {
+    let token = jwt.sign({ email: "mail@ankit-dev.com" }, "secret-token");
+    res.cookie('token', token);
+    res.send('Jwt token set done !');
 });
 
+app.get('/about', (req, res) => {
+    let data = jwt.verify(req.cookies.token, "secret-token");
+    console.log(data);
+});
 
-app.listen(3000);
+app.listen(3000, (req, res) => {
+    console.log('Server is running on port 3000');
+
+});
