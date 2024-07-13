42625ad87483ba42aacbc64fa2ca07624172a8f0
Ankitmaurya-1 <ankitmaurya9560@gmail.com>
2024-07-13 12:29:11 +0530

    EDIT FEATURE

--- 12thVideo/app.js
+++ 12thVideo/app.js
@@ -3,27 +3,45 @@ const app = express();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 const userModel = require("./models/user");
 const postModel = require("./models/post");
 
 // Middleware to parse JSON request bodies
-
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
+mongoose.connect('mongodb://127.0.0.1:27017/miniproject');
 
 app.get('/', (req, res) => {
     res.render('index');
 });
-app.get('/profile', isLoggedIn, (req, res) => {
 
+app.get('/profile', isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email }).populate("posts");
+
+    res.render('profile', { user });
 });
+
+app.post('/post', isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email });
+
+    let post = await postModel.create({
+        user: user._id,
+        content: req.body.content, // Corrected this line
+    });
+
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect('/profile');
+});
+
 app.get('/login', (req, res) => {
     res.render('login');
 });
+
 app.post('/register', async (req, res) => {
     let { email, password, username, name, age } = req.body;
     let user = await userModel.findOne({ email });
@@ -43,10 +61,11 @@ app.post('/register', async (req, res) => {
 
             let token = jwt.sign({ email: email, userid: user._id }, "token");
             res.cookie("token", token);
-            res.send("Registred");
+            res.redirect("/profile");
         });
     });
 });
+
 app.post('/login', async (req, res) => {
     let { email, password } = req.body;
     let user = await userModel.findOne({ email });
@@ -58,7 +77,7 @@ app.post('/login', async (req, res) => {
         if (result) {
             let token = jwt.sign({ email: email, userid: user._id }, "token");
             res.cookie("token", token);
-            res.send("Logged in");
+            res.redirect('/profile');
         } else {
             res.status(400).send('something is wrong');
         }
@@ -70,12 +89,28 @@ app.get('/logout', (req, res) => {
     res.redirect('/login');
 });
 
+app.post('/delete-post/:id', isLoggedIn, async (req, res) => {
+    let postId = req.params.id;
+    let user = await userModel.findOne({ email: req.user.email });
+
+    // Remove the post from the user's posts array
+    user.posts.pull(postId);
+    await user.save();
+
+    // Delete the post from the posts collection
+    await postModel.findByIdAndDelete(postId);
+
+    res.redirect('/profile');
+});
+
+
+
 function isLoggedIn(req, res, next) {
     let token = req.cookies.token;
-    if (!token) return res.send('you must be logged in');
+    if (!token) return res.redirect('/login');
     else {
-
-        res.send("you are now logged in");
+        let data = jwt.verify(token, "token");
+        req.user = data;
     }
     next();
 }
